2715b560b7fa7ab4b591a08b2f6862340e6368f8
liujinkun2025 <77097548+liujinkun2025@users.noreply.github.com>
2026-04-09 00:06:57 +0800

    add wiki node create shortcut (#320)
    Change-Id: I4810fc541c31ae9e3e08539d4b1c91d01f53b7f5

---
+++ shortcuts/wiki/shortcuts.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2026 Lark Technologies Pte. Ltd.
+// SPDX-License-Identifier: MIT
+
+package wiki
+
+import "github.com/larksuite/cli/shortcuts/common"
+
+// Shortcuts returns all wiki shortcuts.
+func Shortcuts() []common.Shortcut {
+	return []common.Shortcut{
+		WikiNodeCreate,
+	}
+}
